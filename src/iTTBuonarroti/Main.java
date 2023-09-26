4c04c3601037a277e558a422e8e157ed3cfcb560
andrea <andrea.boscarato@buonarroti.tn.it>
2023-09-26 18:46:59 +0200

    Commit Finito Compito Classe

--- src/iTTBuonarroti/Main.java
+++ src/iTTBuonarroti/Main.java
@@ -1,6 +1,7 @@
 package iTTBuonarroti;
 
 
+import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -8,28 +9,58 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
 
-        Classe Cl1 = null;
+        Classe classe1 = null;
+        Studente capoclasse = null;
         int lunghezza = 0;
 
-        boolean checkGestRacc = false;
+        System.out.print("\nInserire la lunghezza dell'array: ");
+        input = new Scanner(System.in);
+        lunghezza = input.nextInt();
+
+        Studente[] arrayStudenti = new Studente[lunghezza];
+        boolean checkArray = false;
 
         do{
             try {
-                while()
-                System.out.print("\nInserire la durata della raccolta: ");
-                input = new Scanner(System.in);
-                lunghezza = input.nextInt();
+                for(int i = 0; i < lunghezza; i++){
+                    System.out.print("\nInserire il nome dello studente: ");
+                    input = new Scanner(System.in);
+                    String nome = input.nextLine();
+                    arrayStudenti[i].setNome(nome);
 
-                Cl1 = new Classe();
-                checkGestRacc = true;
+                    System.out.print("\nInserire il cognome dello studente: ");
+                    input = new Scanner(System.in);
+                    String cognome = input.nextLine();
+                    arrayStudenti[i].setNome(cognome);
+                }
+
+                System.out.print("\nInserire il nome dello studente che fara` da capo classe: ");
+                input = new Scanner(System.in);
+                String nome = input.nextLine();
+                capoclasse.setNome(nome);
+
+                System.out.print("\nInserire il cognome dello studente che fara` da capo classe: ");
+                input = new Scanner(System.in);
+                String cognome = input.nextLine();
+                capoclasse.setNome(cognome);
+
+                classe1 = new Classe(capoclasse, arrayStudenti);
+                checkArray = true;
             }
             catch (InputMismatchException e) {
-                System.out.println("\nInserire solo numeri!");
+                System.out.println("\nInserire solo lettere!");
             }
             catch(Exception e) {
                 System.out.println("\nNon va bene: " + e.getMessage());
             }
-        }while(!checkGestRacc);
+        }while(!checkArray);
+
+
+
+        System.out.println("La classe al momento attuale e`: " + Arrays.toString(arrayStudenti));
+        System.out.println("Il capoclasse e`: " + classe1.getCapoClasse());
+
+        classe1.invertiAttributi();
+        System.out.println("\nLa classe sistemata e con nomi e cognomi invertitie`: " + classe1.toString());
     }
 }
-
