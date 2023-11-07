8027b3d8ff17ab78d084f147d20d4027f2155140
Andrea Boscarato <145672198+4DS-boscarato@users.noreply.github.com>
2023-11-07 10:48:46 +0100

    Update Scultura.java

--- src/Ereditarieta/OP/Scultura.java
+++ src/Ereditarieta/OP/Scultura.java
@@ -1,5 +1,28 @@
 package Ereditarieta.OP;
 
-public class Scultura {
+public class Scultura extends OperaDarte {
+    private float altezza;
+    private float larghezza;
 
+    public Quadro(String tit, String art, float alt, float larg) throws Exception {
+        super(tit, art);
+        altezza = alt;
+        larghezza = larg;
+    }
+
+    public float printIngombro(){
+        return altezza * larghezza;
+    }
+
+    public float getAltezza() {
+        return altezza;
+    }
+
+    public float getLarghezza() {
+        return larghezza;
+    }
+
+    public String toString(){
+        return super.toString() + " Altezza Quadro: " +  altezza + " Larghezza Quadro: " + larghezza;
+    }
 }
